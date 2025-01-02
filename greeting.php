f643cd7ad5d736ced3bf1c28b993bc7bba77244f
HunDavin <davin.hun@student.cadt.edu.kh>
2025-01-02 13:38:48 +0700

    homework

--- greeting.php
+++ greeting.php
@@ -1,4 +1,23 @@
-<?php 
-    echo("hello");
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Introduction</title>
+</head>
+<body>
+    <?php
+        echo '<div class ="container m-auto mt-3">';
+        echo '<h1 class="col-9 text-center border-bottom">Introduction Myself</h1>';
+        echo '<h4 class="mt-4">Name: Hun Davin</h4>';
+        echo "<h4>Sex: Male</h4>";
+        echo "<h4>Email: vinnvinnie508@gmail.com</h4>";
+        echo "<h4>Phone number: 0975446207</h4>";
+        echo "<h4>University: CADT</h4>";
+        echo "<h4>Year: 2</h4>";
+        echo "<h4>Major: Software Engineer</h4>";
+        echo '</div>';
+    ?>
+</body>
+</html>
